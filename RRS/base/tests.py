7a9ee95eb50a5c9947d2fdf63673880362e6ebba
Marc Lopez <marc.rainier.lopez@gmail.com>
2015-03-17 10:13:58 -0400

    Fixed Issue: Customer template does not exist Fix: Added customer to installed apps.

--- RRS/base/tests.py
+++ RRS/base/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+import django.test as django_test
 
 # Create your tests here.
+
+class TestForDeadLinks(django_test.TestCase):
+    
+    def test__base_templates__are_online(self):
+        c = django_test.Client()
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200)
