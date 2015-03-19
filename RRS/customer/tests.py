d0ac63484f4006315b968c55ecdede214e3440f9
Marc Lopez <marc.rainier.lopez@gmail.com>
2015-03-19 16:54:42 -0400

    Modified view restaurant test to properly return Http Ok code

--- RRS/customer/tests.py
+++ RRS/customer/tests.py
@@ -4,5 +4,5 @@ class LinksSanityTests(django_test.TestCase):
     
     def test__view_restaurant__is_alive(self):
         c = django_test.Client()
-        response = c.get('/view/1')
+        response = c.get('/view/1/')
         self.assertEqual(response.status_code, 200)
