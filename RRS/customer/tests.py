e4248f2121f13d02e16784201fb91d5fb5e3070b
Marc Lopez <marc.rainier.lopez@gmail.com>
2015-03-22 11:55:31 -0400

    Used reverse urlresolver to get links for unit tests

--- RRS/customer/tests.py
+++ RRS/customer/tests.py
@@ -1,8 +1,12 @@
 import django.test as django_test
+from django.core.urlresolvers import reverse
 
 class LinksSanityTests(django_test.TestCase):
     
     def test__view_restaurant__is_alive(self):
         c = django_test.Client()
-        response = c.get('/view/1/')
+        response = c.get(
+            reverse(
+                'customer:view-restaurant',
+                args=[1]))
         self.assertEqual(response.status_code, 200)
