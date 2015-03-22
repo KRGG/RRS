4affcb4a8cf39bac9d10de3a114c306cbb491580
Marc Lopez <marc.rainier.lopez@gmail.com>
2015-03-21 20:49:51 -0400

    Used process-wrapper to run manage.py runserver

--- RRS/run_blackbox_tests.py
+++ RRS/run_blackbox_tests.py
@@ -1,25 +1,10 @@
 import os
-import subprocess
-import signal
-import psutil
 import robot
+from processwrapper import run_process
 
-def kill_proc_tree(pid, including_parent=True):    
-    parent = psutil.Process(pid)
-    for child in parent.children(recursive=True):
-        child.kill()
-    if including_parent:
-        parent.kill()
 
 if __name__ == '__main__':
-    
-    server = subprocess.Popen(
-        'python manage.py runserver'.split(),
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
-    
-    for f in os.listdir('blackbox_tests'):
-        if os.path.splitext(f)[1] == '.robot':
-            robot.run('blackbox_tests/{}'.format(f))
-    
-    kill_proc_tree(server.pid)
+    with run_process('python manage.py runserver'):
+        for f in os.listdir('blackbox_tests'):
+            if os.path.splitext(f)[1] == '.robot':
+                robot.run('blackbox_tests/{}'.format(f))
