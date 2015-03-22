9c5f787f5f0d21419711624eb8f87a8e1c5d2f77
csyu1 <carl.earvin.yu@gmail.com>
2015-03-23 07:41:41 +0800

    login and signup uses django to generate forms

--- RRS/accounts/forms.py
+++ RRS/accounts/forms.py
@@ -1,14 +1,13 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
+    email = forms.EmailField()
     password = forms.CharField(widget= forms.PasswordInput())
     
 class SignUpForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget= forms.PasswordInput())
-    conf_password = forms.CharField(widget= forms.PasswordInput())
     first_name = forms.CharField()
     last_name = forms.CharField()
     email = forms.EmailField()
+    password = forms.CharField(widget= forms.PasswordInput())
+    confirm_password = forms.CharField(widget= forms.PasswordInput())
     mobile = forms.CharField()
--- RRS/accounts/views.py
+++ RRS/accounts/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from accounts import forms
 
 # Create your views here.
 def login(request):
-    context = {}
+    context={}
+    if request.method =="GET":
+        form = forms.LoginForm()
+    context["form"] = form
     return render(request, 'accounts/login.html', context)
 
     
 def signup(request):
     context = {}
+    if request.method == "GET":
+        form = forms.SignUpForm()
+    context["form"] = form
     return render(request, 'accounts/signup.html', context)
