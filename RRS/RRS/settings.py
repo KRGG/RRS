d24e7c1205f61a5d88a4691d40e2e97eae157a04
csyu1 <carl.earvin.yu@gmail.com>
2015-03-24 07:20:45 +0800

    Uses User Creation Form

--- RRS/RRS/settings.py
+++ RRS/RRS/settings.py
@@ -67,6 +67,8 @@ DATABASES = {
     }
 }
 
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
--- RRS/accounts/views.py
+++ RRS/accounts/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 from accounts import forms
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
-from staff import utils
+from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse
+
+from staff import utils
 
 # Create your views here.
 def login(request):
@@ -22,9 +24,11 @@ def login(request):
                 print "TODO: authenticate and login user2"
                 if user.is_active:
                     login(request, user)
-                    #redirect
+                    return HttpResponseRedirect(reverse('index'))
                 else:
                     context["error"] = "Username and password combination is invalid."
+            else:
+                context["error"] = "User does not exist."
         else:
             context["error"] = "Invalid login."
     context["form"] = form
@@ -38,19 +42,12 @@ def signup(request):
     if request.method == "POST":
         form = forms.SignUpForm(request.POST)
         if form.is_valid():
-            if utils.username_present(form.cleaned_data["username"]):
-                context["error"] = "User already exists."
-            elif utils.email_present(form.cleaned_data["email"]):
-                context["error"] = "Email is already in use."
-            elif form.cleaned_data["confirm_password"] != form.cleaned_data["password"]:
-                context["error"] = "Passwords do not match."
-            else:
-                user = User.objects.create_user(form.cleaned_data["username"], form.cleaned_data["email"], 
-                                                form.cleaned_data["password"])
-                user.first_name = form.cleaned_data["first_name"]
-                user.last_name = form.cleaned_data["last_name"]
-                user.save()
-                return HttpResponseRedirect(reverse('index'))
+            user = User.objects.create_user(form.cleaned_data["username"], form.cleaned_data["email"], 
+                                            form.cleaned_data["password1"])
+            user.first_name = form.cleaned_data["first_name"]
+            user.last_name = form.cleaned_data["last_name"]
+            user.save()
+            return HttpResponseRedirect(reverse('index'))
         else:
             context["error"] = "Please check the details you submitted."
     context["form"] = form
