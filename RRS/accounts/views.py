58f506e7c3386c5d478df95d11a502439a0e2a48
csyu1 <carl.earvin.yu@gmail.com>
2015-03-23 08:01:27 +0800

    Sign Up Form Validation

--- RRS/accounts/views.py
+++ RRS/accounts/views.py
@@ -14,5 +14,14 @@ def signup(request):
     context = {}
     if request.method == "GET":
         form = forms.SignUpForm()
+    if request.method == "POST":
+        form = forms.SignUpForm(request.POST)
+        if form.is_valid():
+            if form.cleaned_data["confirm_password"] == form.cleaned_data["password"]:
+                print "Yay"
+            else:
+                context["error"] = "Passwords do not match."
+        else:
+            context["error"] = "Please check the details you submitted."
     context["form"] = form
     return render(request, 'accounts/signup.html', context)
