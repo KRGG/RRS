2085661d60b539690f2b6d31484e58a206629e95
csyu1 <carl.earvin.yu@gmail.com>
2015-03-23 08:20:36 +0800

    Refactored SignUp to user User Model in Django
    - Uses email as username

--- RRS/accounts/forms.py
+++ RRS/accounts/forms.py
@@ -1,13 +1,18 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget= forms.PasswordInput())
     
-class SignUpForm(forms.Form):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    email = forms.EmailField()
-    password = forms.CharField(widget= forms.PasswordInput())
-    confirm_password = forms.CharField(widget= forms.PasswordInput())
-    mobile = forms.CharField()
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'password')
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+    email = forms.EmailField(max_length=64, label="Email")
+
+    def clean_email( self ):
+        email= self.cleaned_data['username']
+        return email
--- RRS/accounts/views.py
+++ RRS/accounts/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from accounts import forms
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def login(request):
     context={}
     if request.method =="GET":
         form = forms.LoginForm()
+    if request.method =="POST":
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            print "Yay"
+            #authenticate user
+            user = authenticate(user=form.cleaned_data["email"],
+                                password=form.cleaned_data["password"])
     context["form"] = form
     return render(request, 'accounts/login.html', context)
 
@@ -14,6 +22,7 @@ def signup(request):
     context = {}
     if request.method == "GET":
         form = forms.SignUpForm()
+        print form
     if request.method == "POST":
         form = forms.SignUpForm(request.POST)
         if form.is_valid():
