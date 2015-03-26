c10912ec48248be9e7f3f503ce062d0fbf287536
Marc Lopez <marc.rainier.lopez@gmail.com>
2015-03-25 20:35:20 -0400

    Changed sample restaurant id to be a variable

--- RRS/customer/tests.py
+++ RRS/customer/tests.py
@@ -3,9 +3,11 @@ from helpers import LinkSanityTestCase
 class CustomerLinksSanityTests(LinkSanityTestCase):
     
     def test__required_links__are_alive(self):
+        sample_restaurant_id = 1
+        
         self.assert_valid_link(
-            expected_url='/restaurant/1/',
+            expected_url='/restaurant/{}/'.format(sample_restaurant_id),
             url_name='customer:restaurant',
-            args=[1]
+            args=[sample_restaurant_id]
         )
         
