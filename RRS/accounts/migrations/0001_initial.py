21d9b62826045044afa68278e7aa91c3f5700643
Marc Lopez <marc.rainier.lopez@gmail.com>
2015-04-09 15:02:56 -0400

    Fixed db migration issues that causes unit tests to fail

--- RRS/accounts/migrations/0001_initial.py
+++ RRS/accounts/migrations/0001_initial.py
@@ -2,37 +2,56 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('base', '0002_auto_20150325_1702'),
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('auth', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Customer',
+            name='CustomerProfile',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
             options={
+                'abstract': False,
             },
             bases=(models.Model,),
         ),
         migrations.CreateModel(
-            name='Staff',
+            name='StaffProfile',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('restaurant', models.ForeignKey(to='base.Restaurant')),
-                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'permissions': (('edit_restaurant', "Can edit a restaurant's details"),),
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='UserProfile',
+            fields=[
+            ],
+            options={
+                'proxy': True,
+            },
+            bases=('auth.user',),
+        ),
+        migrations.AddField(
+            model_name='customerprofile',
+            name='user',
+            field=models.OneToOneField(to='accounts.UserProfile'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='staffprofile',
+            name='user',
+            field=models.OneToOneField(to='accounts.UserProfile'),
+            preserve_default=True,
         ),
     ]
