c5c3d75d33d1ebede16f405deec80b035c6cf200
noelsison2 <nysison@gmail.com>
2015-04-12 20:56:24 +0800

    Added single profile constrain, permissions and minor refactors

--- RRS/accounts/models.py
+++ RRS/accounts/models.py
@@ -6,26 +6,45 @@ class UserProfile(User):
         proxy = True
 
     def is_customer(self):
-        return len(self.groups.filter(name='Customer')) > 0
+        return hasattr(self, 'customerprofile')
     
     def is_restaurant_staff(self):
-        return len(self.groups.filter(name='Staff')) > 0
+        return hasattr(self, 'staffprofile')
     
 class UserTypeProfile(models.Model):
     class Meta:
         abstract = True
         
-    user = models.OneToOneField(UserProfile)
+    user = models.OneToOneField(UserProfile, primary_key=True)
     
     def __unicode__(self):
         return self.user.username
 
 class CustomerProfile(UserTypeProfile):
-    pass
+    class Meta:
+        permissions = (
+            ("make_reservation", "Can make personal reservations"),
+        )
+
+    def clean(self):
+        if hasattr(self.user, 'staffprofile'):
+            raise ValidationError('User already has a staff profile')
+
 
 class StaffProfile(UserTypeProfile):
     class Meta:
         permissions = (
-            ("edit_restaurant", "Can edit a restaurant's details"),
+            ("make_manual_reservation", "Can make manual reservations"),
+            ("edit_restaurant", "Can edit restaurant's details"),
+            ("edit_menus", "Can edit restaurant's menus"),
+            ("manage_staff", "Can manage restaurant's staff accounts"),
+            ("customer_list", "Can view a list of restaurant's customers"),
+            ("previous_reservations",
+             "Can view a restaurant's reservation history"),
         )
+
     restaurant = models.ForeignKey('base.Restaurant')
+
+    def clean(self):
+        if hasattr(self.user, 'customerprofile'):
+            raise ValidationError('User already has a customer profile')
