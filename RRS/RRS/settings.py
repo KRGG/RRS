12243eed5aba7ff7add7e3828d9ea8c718aaee3d
noelsison2 <nysison@gmail.com>
2015-04-12 22:47:05 +0800

    Working twitter login

--- RRS/RRS/settings.py
+++ RRS/RRS/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = (
     'allauth.socialaccount',
     # 'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.google',
-    # 'allauth.socialaccount.providers.twitter',
+    'allauth.socialaccount.providers.twitter',
 )
 
 # 3rd party app constants
