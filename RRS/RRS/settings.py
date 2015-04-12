f90af77c017aa3d89d4fa201a3e28206c07920da
noelsison2 <nysison@gmail.com>
2015-04-12 23:24:15 +0800

    Fixed brackets and removed reauthentication for facebook

--- RRS/RRS/settings.py
+++ RRS/RRS/settings.py
@@ -138,13 +138,14 @@ STATICFILES_DIRS = (
 SOCIALACCOUNT_QUERY_EMAIL = True
 
 SOCIALACCOUNT_PROVIDERS = \
-    { 'google':
-        { 'SCOPE': ['profile', 'email'],
-          'AUTH_PARAMS': { 'access_type': 'online' },
-      'facebook':
-       {'SCOPE': ['email', 'public_profile', 'user_friends'],
-        'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
-        'METHOD': 'oauth2',
-        'LOCALE_FUNC': lambda request: 'en_US',
-        'VERIFIED_EMAIL': False,
-        'VERSION': 'v2.3'} }}
+    {
+        'google':
+        {'SCOPE': ['profile', 'email'],
+         'AUTH_PARAMS': {'access_type': 'online'}},
+        'facebook':
+        {'SCOPE': ['email', 'public_profile', 'user_friends'],
+            'METHOD': 'oauth2',
+            'LOCALE_FUNC': lambda request: 'en_US',
+            'VERIFIED_EMAIL': False,
+            'VERSION': 'v2.3'}
+    }
