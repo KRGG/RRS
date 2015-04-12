fd3a4ed7b2418b2bc9a02ec98faa9740c65a5ec9
Marc Lopez <marc.rainier.lopez@gmail.com>
2015-04-12 19:15:38 -0400

    Merged database changes from patch

--- RRS/accounts/migrations/0001_initial.py
+++ RRS/accounts/migrations/0001_initial.py
@@ -7,32 +7,11 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0002_auto_20150325_1702'),
         ('auth', '0001_initial'),
+        ('base', '0002_auto_20150325_1702'),
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='CustomerProfile',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-            ],
-            options={
-                'abstract': False,
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='StaffProfile',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('restaurant', models.ForeignKey(to='base.Restaurant')),
-            ],
-            options={
-                'permissions': (('edit_restaurant', "Can edit a restaurant's details"),),
-            },
-            bases=(models.Model,),
-        ),
         migrations.CreateModel(
             name='ExtendedUser',
             fields=[
@@ -42,16 +21,25 @@ class Migration(migrations.Migration):
             },
             bases=('auth.user',),
         ),
-        migrations.AddField(
-            model_name='customerprofile',
-            name='user',
-            field=models.OneToOneField(to='accounts.ExtendedUser'),
-            preserve_default=True,
+        migrations.CreateModel(
+            name='StaffProfile',
+            fields=[
+                ('user', models.OneToOneField(primary_key=True, serialize=False, to='accounts.ExtendedUser')),
+                ('restaurant', models.ForeignKey(to='base.Restaurant')),
+            ],
+            options={
+                'permissions': (('make_manual_reservation', 'Can make manual reservations'), ('edit_restaurant', "Can edit restaurant's details"), ('edit_menus', "Can edit restaurant's menus"), ('manage_staff', "Can manage restaurant's staff accounts"), ('customer_list', "Can view a list of restaurant's customers"), ('previous_reservations', "Can view a restaurant's reservation history")),
+            },
+            bases=(models.Model,),
         ),
-        migrations.AddField(
-            model_name='staffprofile',
-            name='user',
-            field=models.OneToOneField(to='accounts.ExtendedUser'),
-            preserve_default=True,
+        migrations.CreateModel(
+            name='CustomerProfile',
+            fields=[
+                ('user', models.OneToOneField(primary_key=True, serialize=False, to='accounts.ExtendedUser')),
+            ],
+            options={
+                'permissions': (('make_reservation', 'Can make personal reservations'),),
+            },
+            bases=(models.Model,),
         ),
     ]
