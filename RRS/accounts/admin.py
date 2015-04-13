eb690ea2fbd113ff22a036f9d3d689cb207f62fb
Marc Lopez <marc.rainier.lopez@gmail.com>
2015-04-12 22:02:07 -0400

    Refactored profile types to be a choice field

--- RRS/accounts/admin.py
+++ RRS/accounts/admin.py
@@ -3,11 +3,4 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from accounts.models import *
 
-class UserProfileAdmin(UserAdmin):
-    list_display = ('username', 'email', 'is_customer', 'is_restaurant_staff')
-    list_filter = ('is_staff', 'is_superuser', 'groups')
-
-admin.site.unregister(User)
-admin.site.register(ExtendedUser, UserProfileAdmin)
-admin.site.register(CustomerProfile)
-admin.site.register(StaffProfile)
+admin.site.register(UserProfile)
--- RRS/accounts/migrations/0001_initial.py
+++ RRS/accounts/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
@@ -15,30 +16,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ExtendedUser',
             fields=[
+                ('user', models.OneToOneField(primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+                ('type', models.IntegerField(default=1, choices=[(1, b'Customer'), (2, b'Staff')])),
+                ('restaurant', models.ForeignKey(blank=True, to='base.Restaurant', null=True)),
             ],
             options={
-                'proxy': True,
-            },
-            bases=('auth.user',),
-        ),
-        migrations.CreateModel(
-            name='StaffProfile',
-            fields=[
-                ('user', models.OneToOneField(primary_key=True, serialize=False, to='accounts.ExtendedUser')),
-                ('restaurant', models.ForeignKey(to='base.Restaurant')),
-            ],
-            options={
-                'permissions': (('make_manual_reservation', 'Can make manual reservations'), ('edit_restaurant', "Can edit restaurant's details"), ('edit_menus', "Can edit restaurant's menus"), ('manage_staff', "Can manage restaurant's staff accounts"), ('customer_list', "Can view a list of restaurant's customers"), ('previous_reservations', "Can view a restaurant's reservation history")),
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='CustomerProfile',
-            fields=[
-                ('user', models.OneToOneField(primary_key=True, serialize=False, to='accounts.ExtendedUser')),
-            ],
-            options={
-                'permissions': (('make_reservation', 'Can make personal reservations'),),
             },
             bases=(models.Model,),
         ),
