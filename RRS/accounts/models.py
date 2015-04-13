6aef0aa8b89d6d551659d6f8fa4ecd2a1a5986af
Marc Lopez <marc.rainier.lopez@gmail.com>
2015-04-12 22:02:07 -0400

    Refactored profile types to be a choice field

--- RRS/accounts/models.py
+++ RRS/accounts/models.py
@@ -1,50 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class UserProfile(User):
-    class Meta:
-        proxy = True
+class UserProfile(models.Model):
+    
+    permissions = (
+        ("make_reservation", "Can make personal reservations"),
+        ("make_manual_reservation", "Can make manual reservations"),
+        ("edit_restaurant", "Can edit restaurant's details"),
+        ("edit_menus", "Can edit restaurant's menus"),
+        ("manage_staff", "Can manage restaurant's staff accounts"),
+        ("customer_list", "Can view a list of restaurant's customers"),
+        ("previous_reservations",
+         "Can view a restaurant's reservation history"),
+    )
+    CUSTOMER= 1
+    STAFF= 2
+    PROFILE_TYPES= (
+        (CUSTOMER, 'Customer'),
+        (STAFF, 'Staff'),
+    )
+    user = models.OneToOneField(User, primary_key=True)
+    type = models.IntegerField(choices=PROFILE_TYPES, default=CUSTOMER)
+    restaurant = models.ForeignKey('base.Restaurant', blank=True, null=True)
 
     def is_customer(self):
-        return hasattr(self, 'customerprofile')
+        return self.type == self.CUSTOMER
     
     def is_restaurant_staff(self):
-        return hasattr(self, 'staffprofile')
-    
-class UserTypeProfile(models.Model):
-    class Meta:
-        abstract = True
-        
-    user = models.OneToOneField(UserProfile, primary_key=True)
+        return self.type == self.STAFF
     
     def __unicode__(self):
         return self.user.username
-
-class CustomerProfile(UserTypeProfile):
-    class Meta:
-        permissions = (
-            ("make_reservation", "Can make personal reservations"),
-        )
-
-    def clean(self):
-        if hasattr(self.user, 'staffprofile'):
-            raise ValidationError('User already has a staff profile')
-
-
-class StaffProfile(UserTypeProfile):
-    class Meta:
-        permissions = (
-            ("make_manual_reservation", "Can make manual reservations"),
-            ("edit_restaurant", "Can edit restaurant's details"),
-            ("edit_menus", "Can edit restaurant's menus"),
-            ("manage_staff", "Can manage restaurant's staff accounts"),
-            ("customer_list", "Can view a list of restaurant's customers"),
-            ("previous_reservations",
-             "Can view a restaurant's reservation history"),
-        )
-
-    restaurant = models.ForeignKey('base.Restaurant')
-
-    def clean(self):
-        if hasattr(self.user, 'customerprofile'):
-            raise ValidationError('User already has a customer profile')
--- RRS/accounts/tests.py
+++ RRS/accounts/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-from django.contrib.auth.models import Group
-from accounts import models
+from django.contrib.auth.models import User, Group
+from accounts.models import UserProfile
 
 class UserProfileTests(TestCase):
     fixtures = ['db.json']
@@ -9,24 +9,25 @@ class UserProfileTests(TestCase):
 
     def setUp(self):
         USER_ID = 0
-        self.test_user = models.ExtendedUser(USER_ID)
+        self.test_user = User(USER_ID)
+        self.test_profile = UserProfile(user = self.test_user)
         self.test_user.save()
 
     def test__user_profile__returns_true_on_is_customer_if_part_of_customers_group(self): 
-        self._add_to_group(self.CUSTOMER_GROUP)
-        self.assertEqual(self.test_user.is_customer(), True)
+        self.test_profile.type = UserProfile.CUSTOMER
+        self.assertEqual(self.test_profile.is_customer(), True)
         
     def test__staff_profile__returns_true_on_is_staff_if_part_of_staff_group(self): 
-        self._add_to_group(self.STAFF_GROUP)
-        self.assertEqual(self.test_user.is_restaurant_staff(), True)
+        self.test_profile.type = UserProfile.STAFF
+        self.assertEqual(self.test_profile.is_restaurant_staff(), True)
 
     def test__user_profile__returns_false_on_is_customer_if_not_part_of_customers_group(self):
-        self._assert_not_in_group(self.CUSTOMER_GROUP)
-        self.assertEqual(self.test_user.is_customer(), False)
+        self.test_profile.type = UserProfile.STAFF
+        self.assertEqual(self.test_profile.is_customer(), False)
         
     def test__user_profile__returns_false_on_is_staff_if_part_of_staff_group(self):
-        self._assert_not_in_group(self.STAFF_GROUP)
-        self.assertEqual(self.test_user.is_restaurant_staff(), False)
+        self.test_profile.type = UserProfile.CUSTOMER
+        self.assertEqual(self.test_profile.is_restaurant_staff(), False)
         
     def _add_to_group(self, group_name):
         customers = Group.objects.get(name=group_name)
