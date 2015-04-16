03de8c6bb5c973f8bedebcd058d51c4c5de47252
noelsison2 <nysison@gmail.com>
2015-04-16 21:36:48 +0800

    Added url tests to accounts and added status code parameter to default valid link test

--- RRS/accounts/tests.py
+++ RRS/accounts/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
-# Create your tests here.
+from helpers import LinkSanityTestCase
+
+        
+class SignUpTestCase(LinkSanityTestCase):
+    def test__required_links__are_alive(self):
+        self.assert_valid_link(
+            expected_url='/signup/',
+            url_name='accounts:signup',
+        )
+        
+class LoginTestCase(LinkSanityTestCase):
+    def test__required_links__are_alive(self):
+        self.assert_valid_link(
+            expected_url='/login/',
+            url_name='accounts:login',
+        )
+    
+class LogoutTestCase(LinkSanityTestCase):
+    def test__required_links__are_alive(self):
+        self.assert_valid_link(
+            expected_url='/logout/',
+            url_name='accounts:logout',
+            status_code = 302
+        )
+    
