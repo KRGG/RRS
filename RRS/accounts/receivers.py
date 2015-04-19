70acad51cad6784f0b444923c1f92a215fe9077e
noelsison2 <nysison@gmail.com>
2015-04-19 22:16:24 +0800

    Added sign up signal test and refactored to use constants

--- RRS/accounts/receivers.py
+++ RRS/accounts/receivers.py
@@ -1,21 +1,32 @@
-from allauth.account.signals import user_signed_up
+from allauth.account import signals as allauth_signals
 from django.dispatch import receiver
 
+PROVIDER_FACEBOOK = 'facebook'
+PROVIDER_GOOGLE = 'google'
+PROVIDER_TWITTER = 'twitter'
+FACEBOOK_FIRST_NAME_DATA = 'first_name'
+FACEBOOK_LAST_NAME_DATA = 'last_name'
+GOOGLE_FIRST_NAME_DATA = 'given_name'
+GOOGLE_LAST_NAME_DATA = 'family_name'
+FULL_NAME_DATA = 'name'
 
-@receiver(user_signed_up)
-def user_signed_up_(request, user, sociallogin=None, **kwargs):
-    if sociallogin:
-        if sociallogin.account.provider == 'facebook':
-            user.first_name = sociallogin.account.extra_data['first_name']
-            user.last_name = sociallogin.account.extra_data['last_name']
- 
-        elif sociallogin.account.provider == 'google':
-            user.first_name = sociallogin.account.extra_data['given_name']
-            user.last_name = sociallogin.account.extra_data['family_name']
-            
-        elif sociallogin.account.provider == 'twitter':
-            name = sociallogin.account.extra_data['name']
-            user.first_name = name.split()[0]
-            user.last_name = name.split()[1]
- 
+
+@receiver(allauth_signals.user_signed_up)
+def user_signed_up(request, user, social_login=None, **kwargs):
+    if social_login:
+        user.first_name, user.last_name = get_name_from_social_account(
+            social_login.account)
         user.save()
+
+
+def get_name_from_social_account(account):
+    '''Returns first_name, last_name data of account'''
+    if account.provider == PROVIDER_FACEBOOK:
+        return account.extra_data[FACEBOOK_FIRST_NAME_DATA], account.extra_data[FACEBOOK_LAST_NAME_DATA]
+
+    elif account.provider == PROVIDER_GOOGLE:
+        return account.extra_data[GOOGLE_FIRST_NAME_DATA], account.extra_data[GOOGLE_LAST_NAME_DATA]
+
+    elif account.provider == PROVIDER_TWITTER:
+        name = account.extra_data[FULL_NAME_DATA]
+        return name.split()[0], name.split()[1]
--- RRS/helpers/__init__.py
+++ RRS/helpers/__init__.py
@@ -1,13 +1,15 @@
 from django.test import TestCase, Client
 from django.core.urlresolvers import resolve, reverse
 
+from helpers import status
+
 class LinkSanityTestCase(TestCase):
         
     def assert_valid_link(
             self,
             expected_url='',
             url_name='',
-            status_code = 200,
+            status_code = status.HTTP_200_OK,
             **kwargs):
         reversed_url = reverse(url_name, **kwargs)
         self.assertEqual(expected_url, reversed_url)
