57b82e82b98c57d2e71078c0ac4e3bb186c45a38
Marc Lopez <marc.rainier.lopez@gmail.com>
2015-05-27 21:11:23 -0400

    Fit datepicker for small screens

--- RRS/base/static/js/browser_compatibility.js
+++ RRS/base/static/js/browser_compatibility.js
@@ -3,16 +3,21 @@ var isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navig
 function setResponsiveCss() {
 	if ( isSafari ) {
 		$("#branding").css("height", $(document).height() * 0.55);
+		
 		if ( $("#footer").offset().top < $("#body").offset().top + $("#body").outerHeight())
 		{
 			$("#footer").css("position", "");
 			$("#footer").css("bottom", "");
 		}
+		
 		else if ( $("#footer").offset().top + $("#footer").outerHeight() < $(window).height() )
 		{
 			$("#footer").css("position", "absolute");
 			$("#footer").css("bottom", "0px");
 		}
+		
+		$("#ui-datepicker-div").css("max-width", $(window).width() * 0.7);
+		$("#ui-datepicker-div").css("max-height", $(window).height() * 0.6);
 	}
 };
 
